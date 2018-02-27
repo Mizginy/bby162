f6290ea18c7369beaa0b04b901a1eb6d1180fa46
Mizgin Yıldırım <36603293+Mizginy@users.noreply.github.com>
2018-02-27 22:01:20 +0300

    Delete uygulama01.py

--- uygulama01.py
+++
@@ -1,9 +0,0 @@
-_author_ = "Mizgin Yıldırım"
-
-metin = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.."
-print(len(metin)) #=124
-print(str(metin[0:62]))
-
-#metin = input("İlk yarısını yazdırmak istediğiniz metni yazın. :")
-#metninyarisi = metin[: int(len(metin)/2)]
-#print(metninyarisi)
