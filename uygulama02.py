d4e658ff743a62dc45e9920e1b4bbabe947dce9e
Mizgin Yıldırım <36603293+Mizginy@users.noreply.github.com>
2018-02-28 19:22:29 +0300

    Uygulama02.py

---
+++ uygulama02.py
@@ -0,0 +1,27 @@
+_author_ = "Mizgin Yıldırım"
+
+#saniyeyi dk,sa,gun vs çeviren
+#1 yıl 365 gün olarak alındı
+#1 ay 30 gün olarak alındı
+
+zaman = float(input("Saniye cinsinden zamanı gir: "))
+yıl = zaman // (8760 * 3600)
+zaman = zaman % (8760 * 3600)
+
+ay = zaman // (720 * 3600)
+zaman = zaman % (720 * 3600)
+
+hafta = zaman // (168 * 3600)
+zaman = zaman % (168 * 3600)
+
+gun = zaman // (24 * 3600)
+zaman = zaman % (24 * 3600)
+
+saat = zaman // 3600
+zaman %= 3600
+
+dakika = zaman // 60
+zaman %= 60
+
+saniye = zaman
+print("%d yıl, %d ay, %d hafta, %d gun, %d saat, %d dakika, %d saniye. " % (yıl, ay, hafta, gun, saat, dakika, saniye))
